9a9f1b0b3513599b33472263945993232d325c89
htv-hungpt93 <hungpt@hybrid-technologies.vn>
2022-03-28 09:20:43 +0700

    Refactor code and add class description

--- src/com/learn/java/VideoTsDownloader.java
+++ src/com/learn/java/VideoTsDownloader.java
@@ -10,17 +10,30 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
+/**
+ * =========================================
+ * Download .ts video files for only Window.
+ * It needs FFmpeg to merge .ts files
+ * =========================================
+ * Default of `CompletableFuture` use ForkJoinPool
+ * It's a parallelism level and determined by your system settings or based on your current amount of processors.
+ * If core size = 1, a new Thread is created to run each task
+ * =========================================
+ * Custom pool-size by set execution context to runAsync(runnable, executor) or supplyAsync(runnable, executor)
+ * var executor = Executors.newFixedThreadPool(8)
+ */
 public class VideoTsDownloader {
     public static final int DEFAULT_BUFFER_SIZE = 8192;
+
     public static final String PART_FOLDER_PATH = "G:\\Projects\\ts\\out";
     public static final String RESULT_FOLDER_PATH = "G:\\Projects\\ts\\result";
 
-    public static String firstPath = "dsds";
-    public static String lastPath = ".ts?e=1648409903&l=0&h=121c06d581339e0cad6cade0bd605f7a";
+    public static String firstURLPath = "dsds";
+    public static String lastURLPath = ".ts?e=1648409903&l=0&h=121c06d581339e0cad6cade0bd605f7a";
 
-    public static int START = 17;
-    public static int END   = 73;
-    public static int PER   = 200;
+    public static int LINK_IDX_START = 17;
+    public static int LINK_IDX_END   = 73;
+    public static int GROUP_SIZE     = 200;
 
     public static AtomicInteger counter = new AtomicInteger(0);
 
@@ -34,49 +47,48 @@ public class VideoTsDownloader {
         cleanResultFolder(RESULT_FOLDER_PATH);
         System.out.println("======[ Clean successfully ]==============");
 
+        System.out.println("=========[ Start Download, Store & Merge ]======================");
         downloadAndStore(getUrlList());
         runCmd(makeCommand(PART_FOLDER_PATH, ""), 0);
     }
 
     private void cleanResultFolder(String path) {
-        var file = new File(path);
-        var files = file.listFiles();
-        for(int i = 0; i < files.length; i++) {
-            if (files[i].exists()) {
-                files[i].delete();
+        List.of(new File(path).listFiles()).forEach(file -> {
+            if (file.exists()) {
+                file.delete();
             }
-        }
+        });
     }
 
     private List<String> makeCommand(String folder, String tag) {
-        File file = new File(folder);
-        var allFiles = Arrays.stream(file.listFiles()).sorted((t1, t2) -> {
-            var t1Name = t1.getName();
-            var t2Name = t2.getName();
+        var allFiles = Arrays.stream(new File(folder).listFiles()).sorted((f1, f2) -> {
+            var f1Name = f1.getName();
+            var f2Name = f2.getName();
 
-            var t1Idx = Integer.parseInt(t1Name.split("\\.")[0]);
-            var t2Idx = Integer.parseInt(t2Name.split("\\.")[0]);
+            var f1Idx = Integer.parseInt(f1Name.split("\\.")[0]);
+            var f2Idx = Integer.parseInt(f2Name.split("\\.")[0]);
 
-            return  t1Idx - t2Idx;
+            return  f1Idx - f2Idx;
         }).collect(Collectors.toList());
 
-        String allPath = "";
+        StringBuilder allPath = new StringBuilder();
         List<String> mergedPaths = new ArrayList<>();
         int runner = 0;
 
         for(int i = 0; i < allFiles.size(); i++) {
-            allPath += allFiles.get(i).getPath() + "|";
+            var filePath = allFiles.get(i).getPath();
+            allPath.append(filePath).append("|");
             runner ++;
 
-            if (runner == PER || i == (allFiles.size() - 1)) {
+            var isResetPath = (runner == GROUP_SIZE) || (i == allFiles.size() - 1);
+            if (isResetPath) {
                 mergedPaths.add(allPath.substring(0, allPath.length() - 1));
-                allPath = "";
-                runner = 0;
+                allPath = new StringBuilder();
+                runner  = 0;
             }
         }
 
         List<String> commands = new ArrayList<>();
-        System.out.println("=========== [ Size: " + mergedPaths.size());
         for(int i = 0; i < mergedPaths.size(); i++) {
             commands.add(
                "ffmpeg -i \"concat:" + mergedPaths.get(i) + "\" -c copy \"" + RESULT_FOLDER_PATH + "\\" + i + tag +".ts\""
@@ -89,42 +101,40 @@ public class VideoTsDownloader {
         System.out.println("===========[ Start to merge files ]=======================");
         System.out.println("========[ Commands Size: " + commands.size());
 
-        if (flag < 2) {
-            commands.forEach(command -> {
-                try {
-                    ProcessBuilder builder = new ProcessBuilder("cmd.exe", "/c", command);
-                    builder.redirectErrorStream(true);
-                    Process p = builder.start();
+        commands.forEach(command -> {
+            try {
+                ProcessBuilder builder = new ProcessBuilder("cmd.exe", "/c", command);
+                builder.redirectErrorStream(true);
+                Process p = builder.start();
 
-                    BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
-                    String line;
-                    while (true) {
-                        line = r.readLine();
-                        if (line == null) break;
-                        System.out.println(line);
-                    }
-                } catch (Exception e) {
-                    System.out.println(e.getMessage());
+                BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
+                String line;
+                while (true) {
+                    line = r.readLine();
+                    if (line == null) break;
+                    System.out.println(line);
                 }
-            });
-
-            if (flag == 1) {
-                System.out.println("===========[ DONE ]=============================");
-                return;
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
             }
+        });
 
-            System.out.println("==============[ Merge last time ]=====================");
-            runCmd(makeCommand(RESULT_FOLDER_PATH, "_LAST"), 1);
+        if (flag == 1) {
+            System.out.println("===========[ DONE ]=============================");
+            return;
         }
+
+        System.out.println("==============[ Merge last time ]=====================");
+        runCmd(makeCommand(RESULT_FOLDER_PATH, "_LAST"), 1);
     }
 
     private void downloadAndStore(List<String> urls) throws Exception {
         var futureList = new ArrayList<CompletableFuture<Void>>();
 
-        System.out.println("=========[ Start Download ]======================");
         for(int i = 0; i < urls.size(); i++) {
-            int idx = i + 1;
+            int idx    = i + 1;
             String url = urls.get(i);
+
             var result = CompletableFuture.runAsync(() -> {
                 try {
                     download(url, idx);
@@ -143,11 +153,10 @@ public class VideoTsDownloader {
 
     private void download(String url, int idx) throws Exception {
         File file = new File(PART_FOLDER_PATH + "\\" + idx + ".ts");
+
         URL tsUrl = new URL(url);
         HttpURLConnection connection = (HttpURLConnection) tsUrl.openConnection();
         connection.connect();
-
-        long urlFileLength = connection.getContentLength();
         InputStream input = connection.getInputStream();
 
         try (FileOutputStream outputStream = new FileOutputStream(file, false)) {
@@ -161,8 +170,8 @@ public class VideoTsDownloader {
 
     private List<String> getUrlList() {
         var urls = new ArrayList<String>();
-        for(int i = START; i <= END; i++) {
-            var url = firstPath + i + lastPath;
+        for(int i = LINK_IDX_START; i <= LINK_IDX_END; i++) {
+            var url = firstURLPath + i + lastURLPath;
             urls.add(url);
         }
 
